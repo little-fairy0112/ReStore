83916b663975a2d6fe0603ffc975047b73c80121
LuluHung <chloe.yj.hung@foxconn.com>
2024-05-02 17:12:52 +0800

    End of Section3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // 後端API伺服器domain
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 })
